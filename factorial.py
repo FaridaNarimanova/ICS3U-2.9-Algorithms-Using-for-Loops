53216d9bf4c389d5f01bc57f77586f14da898fae
FaridaNarimanova <farida.narimanova25@ycdsbk12.ca>
2024-03-20 19:39:55 -0400

    solved

--- factorial.py
+++ factorial.py
@@ -5,3 +5,14 @@ Date: 2024-03-19
 Description: A program that asks for user input, n, and calculates the
              factorial n! outputting the final value.
 """
+#I ask uswe for the n number whihc defines the starting number
+n = int(input("numb: "))
+# I assign numb to 1 since by multipling number by 0 i get zero, so numb is 1
+numb = 1
+# i is the variable in which value changes as my integer goes through range, so everytyme it goes through body, it changes 
+# n is starting number
+# 0 i the number i will get to
+# - 1 i subtract n everytime
+for i in range (n, 1 , - 1 ):
+    numb *= i  
+print(numb)
--- sum.py
+++ sum.py
@@ -5,3 +5,18 @@ Date: 2024-03-19
 Description: A program that asks for user input, n, and calculates the
              sum of numbers from 1 to n using an accumulator variable.
 """
+
+
+sum = 0 
+n = int(input("Enter number: "))
+for counter in range(1, n+1):
+    if counter <= n:
+        sum = sum + counter  # I update accumaltion variable sum evrytime
+        counter = counter + 1    # to know whne i need ot stop looping, tilll i get the point where n is less than 1
+    else:
+        print(sum)
+print(sum)
+
+
+    
+
